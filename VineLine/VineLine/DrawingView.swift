42271bc0d9e7b5cf09b2cd803a8b454c6d454427
Michael Behan <michael@marinosoftware.com>
2019-03-17 09:54:40 +0000

    Switch to using UIView's touch handling methods instead of gesture recogniser

--- VineLine/VineLine/DrawingView.swift
+++ VineLine/VineLine/DrawingView.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class DrawingView: UIView {
 
+    let lineWidth = CGFloat(1.0)
+    var activeLines = [UITouch: UIBezierPath]()
     var lines = [UIBezierPath]()
 
     override init(frame: CGRect) {
@@ -23,30 +25,45 @@ class DrawingView: UIView {
     }
 
     func setup(){
-        let panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panHandler(panRecognizer:)))
-        self.addGestureRecognizer(panRecognizer)
-        
+        isMultipleTouchEnabled = true
     }
 
-    @objc func panHandler(panRecognizer: UIGestureRecognizer) {
-
-        switch panRecognizer.state {
-        case .began:
-            let path = UIBezierPath()
-            path.move(to: panRecognizer.location(in: self))
-            lines.append(path)
-        case .changed, .ended, .cancelled:
-            lines.last?.addLine(to: panRecognizer.location(in: self))
-        default: break
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.isMultipleTouchEnabled = true
+        for touch in touches {
+            let location = touch.location(in: self)
+            let newStroke = UIBezierPath()
+            newStroke.lineCapStyle = .round
+            newStroke.lineWidth = lineWidth
+            newStroke.move(to: location)
+            newStroke.addLine(to: location)
+            activeLines[touch] = newStroke
         }
+    }
 
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            activeLines[touch]!.addLine(to: touch.location(in: self))
+        }
+        self.setNeedsDisplay()
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let line = activeLines.removeValue(forKey: touch)!
+            line.move(to: touch.location(in: self))
+            lines.append(line)
+        }
         self.setNeedsDisplay()
     }
 
     override func draw(_ rect: CGRect) {
-        for line in lines {
+        for line in activeLines.values {
+            line.stroke()
+        }
+
+        for line in lines{
             line.stroke()
         }
     }
-
 }
